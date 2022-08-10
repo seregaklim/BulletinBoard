768968c76b97a352f5fa07412614cd65c127338a
Your Name <seregaklim@mail.ru>
2022-08-11 01:07:18 +0300

    ImagePicker

--- app/src/main/java/com/seregaklim/bulletinboard/act/EditAdsAct.kt
+++ app/src/main/java/com/seregaklim/bulletinboard/act/EditAdsAct.kt
@@ -1,36 +1,85 @@
 package com.seregaklim.bulletinboard.act
 
+
 import android.R
+import android.content.Intent
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
-import android.widget.ArrayAdapter
+
 import android.widget.Toast
+import com.fxn.pix.Pix
+
+import com.fxn.utility.PermUtil
 import com.seregaklim.bulletinboard.databinding.ActivityEditAdsBinding
 import com.seregaklim.bulletinboard.dialogs.DialogSpinnerHelper
 import com.seregaklim.bulletinboard.utils.CityHelper
+import com.seregaklim.bulletinboard.utils.ImagePicker
 
 
 class EditAdsAct : AppCompatActivity() {
 
     lateinit var binding: ActivityEditAdsBinding
-private  val dialog= DialogSpinnerHelper()
+    private  val dialog= DialogSpinnerHelper()
+
+   val imagePicker = ImagePicker()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityEditAdsBinding.inflate(layoutInflater)
         setContentView(binding.root)
         init()
+
+    }
+//запрос на обработку картинок
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == RESULT_OK && resultCode == imagePicker.REQUEST_CODE_GET_IMAGES){
+            if (data !=null){
+                val returnValue = data.getStringArrayListExtra(Pix.IMAGE_RESULTS)
+                Log.d("Mylog","Image :${returnValue?.get(0)}")
+                Log.d("Mylog","Image :${returnValue?.get(1)}")
+                Log.d("Mylog","Image :${returnValue?.get(2)}")
+            }
+        }
     }
 
-private fun init(){
-//
+    //запрашиваем разрешение досиупа к фотографиям
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        //   super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when(requestCode){
+            PermUtil.REQUEST_CODE_ASK_MULTIPLE_PERMISSIONS->{
+                if(grantResults.isNotEmpty() && grantResults [0]== PackageManager.PERMISSION_GRANTED){
+                   // isImagesPermissionGranted =true
+               imagePicker.getImages(this)
+                } else {
+                 //   isImagesPermissionGranted =false
+                    Toast.makeText(this,"Включите, разрешение!!", Toast.LENGTH_LONG).show()
+                }
+                return
+            }
+
+        }
+    }
+
+
+
+    private fun init(){
+
 //val listCountry=CityHelper.getAllCountries(this)
 //
 //dialog.showSpinnerDialog(this,listCountry)
-}
+    }
 
     //OnClicks
+    //  ищем страну в DialogSpinnerHelper
     fun onClickSelectCountry(view: View){
         val listCountry = CityHelper.getAllCountries(this)
         dialog.showSpinnerDialog(this, listCountry, binding.tvCountry)
@@ -38,7 +87,7 @@ private fun init(){
             binding.tvCity.text = getString(com.seregaklim.bulletinboard.R.string.select_city)
         }
     }
-
+    //ищем город в DialogSpinnerHelper
     fun onClickSelectCity(view: View){
         val selectedCountry = binding.tvCountry.text.toString()
         if(selectedCountry != getString(com.seregaklim.bulletinboard.R.string.select_country)){
@@ -49,7 +98,15 @@ private fun init(){
         }
     }
 
-
+    //запускаем картинку
+    fun onClickGetImages(view: View){
+//        if(imageAdapter.mainArray.size == 0){
+          imagePicker.getImages(this)
+//        } else {
+//            openChooseImageFrag(null)
+//            chooseImageFrag?.updateAdapterFromEdit(imageAdapter.mainArray)
+//        }
+    }
 
 }
 
---
+++ app/src/main/java/com/seregaklim/bulletinboard/utils/ImagePicker.kt
@@ -0,0 +1,22 @@
+package com.seregaklim.bulletinboard.utils
+
+import android.content.Context
+import androidx.appcompat.app.AppCompatActivity
+import com.fxn.pix.Options
+import com.fxn.pix.Pix
+
+class ImagePicker {
+    val REQUEST_CODE_GET_IMAGES=999
+    fun  getImages(context: AppCompatActivity) {
+        val options = Options.init()
+            .setRequestCode(REQUEST_CODE_GET_IMAGES)
+            .setCount(3)
+            .setFrontfacing(false)
+            .setMode(Options.Mode.Picture)
+            .setScreenOrientation(Options.SCREEN_ORIENTATION_REVERSE_PORTRAIT)
+           .setPath ("/pix/images")
+
+    Pix.start(context,options)
+    }
+
+}
